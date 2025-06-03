bcd43f99b93bdfeb6966fa6825f1b693c047a5fa
zili <neodejack@gmail.com>
2025-06-03 09:50:59 +0800

    npm install path

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -7,3 +7,6 @@ export PATH="$HOME/.local/bin:$PATH"
 
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/opt/mysql-client@8.4/bin:$PATH"
+
+## for npm installed stuff
+export PATH="/Users/zilizhang/.nvm/versions/node/v22.16.0/bin:$PATH"

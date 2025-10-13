eaa4c37953674baa394aff18f400f79a32946fe9
zili <neodejack@gmail.com>
2025-10-13 09:21:42 +0800

    lazyvim sidekick

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,6 @@ return {
   {
     "catppuccin/nvim",
     opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
       opts.color_overrides = {
         all = {
           base = "#171717",

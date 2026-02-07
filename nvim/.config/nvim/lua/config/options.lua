8182cab98228952e6f541a44fb47ba38ba33a87b
zili <neodejack@gmail.com>
2026-02-07 17:36:09 +0800

    fix ctrl-g neovim clipboard

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,21 @@
 -- Add any additional options here
 vim.g.snacks_animate = false
 vim.opt.conceallevel = 0
+
+if not vim.env.SSH_CONNECTION then
+  local clipboard_group = vim.api.nvim_create_augroup("local_clipboard", { clear = true })
+  local function force_unnamedplus()
+    vim.opt.clipboard = "unnamedplus"
+  end
+
+  -- LazyVim defers clipboard setup; enforce unnamedplus when UI is ready.
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = clipboard_group,
+    callback = force_unnamedplus,
+  })
+  vim.api.nvim_create_autocmd("User", {
+    group = clipboard_group,
+    pattern = "VeryLazy",
+    callback = force_unnamedplus,
+  })
+end

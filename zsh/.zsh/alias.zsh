3e4e083b629bd2e9b94bed7743001adf8248b9e8
zili <neodejack@gmail.com>
2026-04-30 10:47:00 +0800

    more alias

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -3,6 +3,7 @@ alias ll="ls -l"
 
 # elixir
 alias ex="elixir"
+alias exdoc="mix hex.docs offline elixir"
 
 #nvim as default
 alias vim="nvim"
@@ -13,17 +14,15 @@ alias k=kubectl
 # lazygit
 alias lg="lazygit"
 
-# codex
-alias cfast="codex -c model_reasoning_effort=low"
-alias c="codex --dangerously-bypass-approvals-and-sandbox"
-
 # just global
 alias j="just -g"
 
 # kill zen
 alias killzen=" pkill -f '/Applications/Zen.app/Contents/MacOS/zen'"
 
-# paper
-alias paper="codex -c mcp_servers.paper.enabled=true --yolo"
+# codex
+alias c="codex --dangerously-bypass-approvals-and-sandbox"
+alias c_low="codex -c model_reasoning_effort=low"
+alias c_paper="codex -c mcp_servers.paper.enabled=true --yolo"
+alias c_chrome="codex -c mcp_servers.chrome-devtools=true --yolo"
 
-alias exdoc="mix hex.docs offline elixir"

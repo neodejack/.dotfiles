e54a1e90754cc8acb3bc135bdcf482775394c660
zili <neodejack@gmail.com>
2026-06-07 17:14:53 +0800

    fix try

--- zsh/.zsh/try.zsh
+++ zsh/.zsh/try.zsh
@@ -1,6 +1,6 @@
 try() {
   local out
-  out=$(/usr/bin/env ruby '/opt/homebrew/bin/try' exec --path "$HOME/code/tries" "$@" 2>/dev/tty)
+  out=$(ruby '/opt/homebrew/Cellar/try/1.9.3/libexec/gems/try-cli-1.9.3/try.rb' exec --path "$HOME/code/personal/tries" "$@" 2>/dev/tty)
   if [ $? -eq 0 ]; then
     eval "$out"
   else
